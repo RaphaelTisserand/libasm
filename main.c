4027b212d011c026997d2de9592ed6b5f30743a2
raph2leretour <tisserandraph@gmail.com>
2025-09-22 19:54:57 +0200

    fixed main.c

--- main.c
+++ main.c
@@ -6,8 +6,8 @@ extern size_t	ft_strlen(char* str);
 
 int	main(int ac, char** av) {
 	char*	str = "merci snaji pour les travaux\n";
-
-	printf("strlen: %d; ft_strlen: %d\n", strlen(str), ft_strlen(str));
+	printf("strlen: %ld; ft_strlen: %ld\n", \
+		strlen(str), ft_strlen(str));
 
 	return 0;
 }
